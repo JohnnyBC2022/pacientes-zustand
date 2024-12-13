efc523550315469816d1db89187bebbdc36fbd37
Johnny <jonathanbc2012@gmail.com>
2024-12-13 19:44:07 +0100

    feat: PatientForm and PatientsList created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import PatientForm from "./components/PatientForm";
+import PatientsList from "./components/PatientsList";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl bg-red-500">Pacientes</h1>
+      <div className="container max-auto mt-20">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+          Seguimiento de {""}
+          <span className="text-amber-600">Pacientes</span>
+        </h1>
+
+        <div className="mt-12 md:flex">
+          <PatientForm/>
+          <PatientsList/>
+        </div>
+      </div>
     </>
   );
 }
