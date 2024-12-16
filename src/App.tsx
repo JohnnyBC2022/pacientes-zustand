8863b26facaf60f3780e62ce76da9aac4f46cd9c
Johnny <jonathanbc2012@gmail.com>
2024-12-16 13:51:35 +0100

    feat: react-toastify added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+import {ToastContainer} from 'react-toastify'
 import PatientForm from "./components/PatientForm";
 import PatientsList from "./components/PatientsList";
+import "react-toastify/ReactToastify.css"
 
 function App() {
   return (
     <>
-      <div className="container max-auto mt-20">
-        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto">
+      <div className="container max-auto mt-20 ">
+        <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto ">
           Seguimiento de {""}
           <span className="text-amber-600">Pacientes</span>
         </h1>
-
         <div className="mt-12 md:flex">
           <PatientForm/>
           <PatientsList/>
         </div>
       </div>
+
+      <ToastContainer/>
     </>
   );
 }
--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -1,8 +1,9 @@
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import {toast} from 'react-toastify'
 import Error from "./Error";
 import type { DraftPatient } from "../types";
 import { usePatientStore } from "../store";
-import { useEffect } from "react";
 
 export default function PatientForm() {
 
@@ -34,6 +35,10 @@ export default function PatientForm() {
       updatePatient(data)
     } else{
       addPatient(data)
+      toast.success('Paciente Registrado Correctamente',{
+        position: 'bottom-right',
+        theme: 'dark'
+      })
     }
 
     reset()
