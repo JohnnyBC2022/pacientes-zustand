5c6f96ece2239cb2fa69b1d3757bb1acce799401
Johnny <jonathanbc2012@gmail.com>
2024-12-16 12:48:43 +0100

    feat: PatientDetailItem created and PatientDetails modified

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,13 +1,21 @@
-import { Patient } from "../types"
+import { Patient } from "../types";
+import PatientDetailItem from "./PatientDetailItem";
 
 type PatientDetailsProps = {
-    patient: Patient
-}
+  patient: Patient;
+};
 
-function PatientDetails({patient}: PatientDetailsProps) {
+function PatientDetails({ patient }: PatientDetailsProps) {
   return (
-    <div>{patient.name}</div>
-  )
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatientDetailItem label="ID" data={patient.id} />
+      <PatientDetailItem label="Nombre Paciente" data={patient.name} />
+      <PatientDetailItem label="Nombre Persona de Contacto" data={patient.contact} />
+      <PatientDetailItem label="Email" data={patient.email} />
+      <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
+      <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+    </div>
+  );
 }
 
-export default PatientDetails
+export default PatientDetails;
