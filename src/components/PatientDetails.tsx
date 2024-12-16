b09d9519d77419c8e3917037d009fcccbe770c0d
Johnny <jonathanbc2012@gmail.com>
2024-12-16 13:37:17 +0100

    feat: update function created

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -9,6 +9,8 @@ type PatientDetailsProps = {
 function PatientDetails({ patient }: PatientDetailsProps) {
 
     const deletePatient = usePatientStore((state) => state.deletePatient)
+    const getPatientById = usePatientStore((state) => state.getPatientById)
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="ID" data={patient.id} />
@@ -18,10 +20,11 @@ function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
       <PatientDetailItem label="Síntomas" data={patient.symptoms} />
 
-      <div className="flex justify-between mt-10">
+      <div className="flex flex-col lg:flex-row gap-3 justify-between mt-10">
         <button
             type="button"
             className="py-2 px-10 bg bg-amber-600 hover:bg-amber-700 text-white font-bold uppercase rounded-lg"
+            onClick={()=>getPatientById(patient.id)}
         >
             Editar
         </button>
