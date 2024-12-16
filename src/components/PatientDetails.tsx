9588c2f93880bb7312d0612d343315c4fe55fedf
Johnny <jonathanbc2012@gmail.com>
2024-12-16 12:55:11 +0100

    feat: buttons editar and eliminar added to patient details. reset function added to PatientForm

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -14,6 +14,22 @@ function PatientDetails({ patient }: PatientDetailsProps) {
       <PatientDetailItem label="Email" data={patient.email} />
       <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
       <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button
+            type="button"
+            className="py-2 px-10 bg bg-amber-600 hover:bg-amber-700 text-white font-bold uppercase rounded-lg"
+        >
+            Editar
+        </button>
+
+        <button
+            type="button"
+            className="py-2 px-10 bg bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+            Eliminar
+        </button>
+      </div>
     </div>
   );
 }
