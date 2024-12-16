507a800fdc5ddabcc799aad49a75ae0f37fecb58
Johnny <jonathanbc2012@gmail.com>
2024-12-16 21:45:10 +0100

    fix: main container centered

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "react-toastify/ReactToastify.css";
 function App() {
   return (
     <>
-      <div className="container max-auto mt-20 ">
+      <div className="container mx-auto mt-20 ">
         <h1 className="font-black text-5xl text-center md:w-2/3 md:mx-auto ">
           Seguimiento de {""}
           <span className="text-amber-600">Pacientes</span>
